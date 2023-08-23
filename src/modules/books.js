dad9ced8da3819cafdbe53f9d0e2e014150e163c
sstfnk3107 <s.stefanyk75@gmail.com>
2023-08-23 22:59:04 +0300

    123

--- src/modules/books.js
+++ src/modules/books.js
@@ -5,81 +5,86 @@
 //  Поиск книг по жанру.
 //  Поиск книг по автору.
 //  Сортировка книг по цене(по возрастанию или
-// убыванию). 
+// убыванию).
 // Пагинация списка книг.
 
-import axios from 'axios'
+import axios from 'axios';
 
 const BASE_URL = `http://localhost:5000`;
 
-export function getAllBooks() {
-    const END_POINT = `/books`;
-    const url = BASE_URL + END_POINT;
-    // axios.get(url).then(res=>res.data)
-    return fetch(url).then(res => res.json());
-};
-
-export function getBooksId(id) {
-   const END_POINT = `/books?id=${id}`;
-    const url = BASE_URL + END_POINT;
-    return fetch(url).then(res => res.json());
-};
-
-export function getBookGenre(genre) {
-    const END_POINT = `/books?genre=${genre}`;
-    const url = BASE_URL + END_POINT;
-    return fetch(url).then(res => res.json());
-};
-
-export function getBookAuthor(author) {
-    const END_POINT = `/books?author=${author}`;
-    const url = BASE_URL + END_POINT;
-    return fetch(url).then(res => res.json());
-};
-
-export function getPageBooks(_page, _limit) {
-    const END_POINT = `/books?_page=${_page}&_limit=${_limit}`;
-    const url = BASE_URL + END_POINT;
-    return fetch(url).then(res => res.json());
-};
-
-export function getBookPrice(price) {
-    const END_POINT = `/books?price=${price}&_sort=amount&_order=desc`;
-    const url = BASE_URL + END_POINT;
-    return fetch(url).then(res => res.json());
-};
-export function createBook(book) { 
-    const options = {
-        method: 'POST', body: JSON.stringify(book),
-        headers: { 'Content-Type': 'application/json' }
-    }
-    const END_POINT = `/books`;
-    const url = BASE_URL + END_POINT;
-    return fetch(url, options).then(res => res.json());
-    axios.post(url, book);
+export async function getAllBooks() {
+  const END_POINT = `/books`;
+  const url = BASE_URL + END_POINT;
+  // axios.get(url).then(res=>res.data)
+  const res = await fetch(url);
+  return res.json();
 }
 
-export function updateBook(id, book) { 
-    const END_POINT = `/books/${id}`;
-    const url = BASE_URL + END_POINT;
-    return axios.patch(url, book).then(res => res.data);
+export async function getBooksId(id) {
+  const END_POINT = `/books?id=${id}`;
+  const url = BASE_URL + END_POINT;
+  const res = await fetch(url);
+  return res.json();
 }
 
-export function resetBook(id, book) { 
-    const END_POINT = `/books/${id}`;
-    const url = BASE_URL + END_POINT;
-    return axios.put(url, book).then(res => res.data);
+export async function getBookGenre(genre) {
+  const END_POINT = `/books?genre=${genre}`;
+  const url = BASE_URL + END_POINT;
+  const res = await fetch(url);
+  return res.json();
 }
 
-export function deleteBook(id) { 
-    const options = {method: 'DELETE'}
-    const END_POINT = `/books/${id}`;
-    const url = BASE_URL + END_POINT;
-    return fetch(url, options).then(res => res.json())
+export async function getBookAuthor(author) {
+  const END_POINT = `/books?author=${author}`;
+  const url = BASE_URL + END_POINT;
+  const res = await fetch(url);
+  return res.json();
+}
 
-};
+export async function getPageBooks(_page, _limit) {
+  const END_POINT = `/books?_page=${_page}&_limit=${_limit}`;
+  const url = BASE_URL + END_POINT;
+  const res = await fetch(url);
+  return res.json();
+}
 
+export async function getBookPrice(price) {
+  const END_POINT = `/books?price=${price}&_sort=amount&_order=desc`;
+  const url = BASE_URL + END_POINT;
+  const res = await fetch(url);
+  return res.json();
+}
+export async function createBook(book) {
+  const options = {
+    method: 'POST',
+    body: JSON.stringify(book),
+    headers: { 'Content-Type': 'application/json' },
+  };
+  const END_POINT = `/books`;
+  const url = BASE_URL + END_POINT;
+  const res = await fetch(url, options);
+  return res.json();
+  // axios.post(url, book);
+}
 
+export async function updateBook(id, book) {
+  const END_POINT = `/books/${id}`;
+  const url = BASE_URL + END_POINT;
+  const res = await axios.patch(url, book);
+  return res.data;
+}
 
+export async function resetBook(id, book) {
+  const END_POINT = `/books/${id}`;
+  const url = BASE_URL + END_POINT;
+  const res = await axios.put(url, book);
+  return res.data;
+}
 
-
+export async function deleteBook(id) {
+  const options = { method: 'DELETE' };
+  const END_POINT = `/books/${id}`;
+  const url = BASE_URL + END_POINT;
+  const res = await fetch(url, options);
+  return res.json();
+}
--- src/modules/cars.js
+++ src/modules/cars.js
@@ -1,68 +1,74 @@
 // Получение всех автомобилей.
 //  Получение информации об автомобиле по его
-// идентификатору. 
+// идентификатору.
 // Поиск автомобилей по марке.
 // Поиск автомобилей по модели.
 // Пагинация списка автомобилей.
 
-import axios from "axios";
+import axios from 'axios';
 
 const BASE_URL = `http://localhost:5000`;
 
-export function getAllCars() {
-    
-    const END_POINT = `/cars`;
-    const URL = BASE_URL + END_POINT;
-    return fetch(URL).then(res => res.json());
-};
+export async function getAllCars() {
+  const END_POINT = `/cars`;
+  const URL = BASE_URL + END_POINT;
+  const res = await fetch(URL);
+  return res.json();
+}
 
-export function getInfoCars(id) {
-      const END_POINT = `/cars?id=${id}`;
-    const URL = BASE_URL + END_POINT;
-    return fetch(URL).then(res => res.json());
-};
+export async function getInfoCars(id) {
+  const END_POINT = `/cars?id=${id}`;
+  const URL = BASE_URL + END_POINT;
+  const res = await fetch(URL);
+  return res.json();
+}
 
-export function getCarsByMarka(marka) {
-    
-       const END_POINT = `/cars?marka=${marka}`;
-    const URL = BASE_URL + END_POINT;
-    return fetch(URL).then(res => res.json());
-};
+export async function getCarsByMarka(marka) {
+  const END_POINT = `/cars?marka=${marka}`;
+  const URL = BASE_URL + END_POINT;
+  const res = await fetch(URL);
+  return res.json();
+}
 
-export function getCarsByModel(model) {
-        const END_POINT = `/cars?model=${model}`;
-    const URL = BASE_URL + END_POINT;
-    return fetch(URL).then(res => res.json());
-};
+export async function getCarsByModel(model) {
+  const END_POINT = `/cars?model=${model}`;
+  const URL = BASE_URL + END_POINT;
+  const res = await fetch(URL);
+  return res.json();
+}
 
-export function getCarsByPage(page, limit ) {
-    const END_POINT = `/cars?_page=${page}&_limit=${limit}`;
-    const URL = BASE_URL + END_POINT;
-    return fetch(URL).then(res => res.json()); 
-};
-
+export async function getCarsByPage(page, limit) {
+  const END_POINT = `/cars?_page=${page}&_limit=${limit}`;
+  const URL = BASE_URL + END_POINT;
+  const res = await fetch(URL);
+  return res.json();
+}
 
 // =====================
-export function createCars(car) {
-    const END_POINT = `/cars`;
-    const url = BASE_URL + END_POINT;
-    return axios.post(url, car).then(res => res.data);
-};
+export async function createCars(car) {
+  const END_POINT = `/cars`;
+  const url = BASE_URL + END_POINT;
+  const res = await axios.post(url, car);
+  return res.data;
+}
 
-export function updateCars(id, car) {
-    const END_POINT = `/cars/${id}`;
-    const url = BASE_URL + END_POINT;
-    return axios.patch(url, car).then(res => res.data);
-};
+export async function updateCars(id, car) {
+  const END_POINT = `/cars/${id}`;
+  const url = BASE_URL + END_POINT;
+  const res = await axios.patch(url, car);
+  return res.data;
+}
 
-export function resetCars(id, car) {
-    const END_POINT = `/cars/${id}`;
-    const url = BASE_URL + END_POINT;
-    return axios.put(url, car).then(res => res.data);
-};
+export async function resetCars(id, car) {
+  const END_POINT = `/cars/${id}`;
+  const url = BASE_URL + END_POINT;
+  const res = await axios.put(url, car);
+  return res.data;
+}
 
-export function deleteCars(id) {
-    const END_POINT = `/cars/${id}`;
-    const url = BASE_URL + END_POINT;
-    return axios.delete(url).then(res => res.data);
-};
+export async function deleteCars(id) {
+  const END_POINT = `/cars/${id}`;
+  const url = BASE_URL + END_POINT;
+  const res = await axios.delete(url);
+  return res.data;
+}
