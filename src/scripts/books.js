2ee7f3cd997f54059848e878bcd018cc1f2f683b
sstfnk3107 <s.stefanyk75@gmail.com>
2023-08-23 22:27:21 +0300

    2345

--- src/scripts/books.js
+++ src/scripts/books.js
@@ -1,13 +1,17 @@
-import { createBook, deleteBook, updateBook, resetBook, getAllBooks } from '../modules/books'
-
-
+import {
+  createBook,
+  deleteBook,
+  updateBook,
+  resetBook,
+  getAllBooks,
+} from '../modules/books';
 
 const refs = {
-    form: document.querySelector('.js-form'),
-    form3: document.querySelector('.js-form3'),
-    form1: document.querySelector('.js-form1'),
-    form2: document.querySelector('.js-form2'),
-    ulList: document.querySelector('.js-bookList')
+  form: document.querySelector('.js-form'),
+  form3: document.querySelector('.js-form3'),
+  form1: document.querySelector('.js-form1'),
+  form2: document.querySelector('.js-form2'),
+  ulList: document.querySelector('.js-bookList'),
 };
 
 refs.form.addEventListener('submit', onCreateBooksSubmit);
@@ -15,26 +19,24 @@ refs.form3.addEventListener('submit', onDEleteBookSubmit);
 refs.form1.addEventListener('submit', onUpdateBookSubmit);
 refs.form2.addEventListener('submit', onResetBookSubmit);
 
+async function onCreateBooksSubmit(evt) {
+  evt.preventDefault();
 
-function onCreateBooksSubmit(evt) {
-    evt.preventDefault();
+  const formData = new FormData(evt.target);
+  const book = {};
+  formData.forEach((value, key) => {
+    book[key] = value;
+  });
 
-    const formData = new FormData(evt.target);
-    const book = {};
-    formData.forEach((value, key) => {
-        book[key] = value;
-    })
+  const book1 = await createBook(book);
+  const murkup = bookTamplate(book1);
+  refs.ulList.insertAdjacentHTML('beforeend', murkup);
 
-    createBook(book).then(book => {
-        const murkup = bookTamplate(book);
-        refs.ulList.insertAdjacentHTML('beforeend', murkup)
-    })
-    evt.target.reset();
-    
-};
+  evt.target.reset();
+}
 
 function bookTamplate({ id, title, author, about, genre, amount, price }) {
-    return `
+  return `
        <li data-id="${id}" class="book">
   <h3 class="book__title">${title}</h3>
   <p class="book__desc">
@@ -47,70 +49,62 @@ function bookTamplate({ id, title, author, about, genre, amount, price }) {
     <span>${amount} - ${price}</span>
     <span> ID = ${id}<span>
   </div>
-</li> `
+</li> `;
 }
 
-function onDEleteBookSubmit(evt) {
-    evt.preventDefault();
-    const deleteBookId = evt.target.elements.id.value;
-    deleteBook(deleteBookId).then(book => {
-        console.log(book);
-    })
-    evt.target.reset();
-    refs.ulList.querySelector(`[data-id="${deleteBookId}"]`).remove();
-};
-function onUpdateBookSubmit(evt) {
-    evt.preventDefault();
+async function onDEleteBookSubmit(evt) {
+  evt.preventDefault();
+  const deleteBookId = evt.target.elements.id.value;
+  const book = await deleteBook(deleteBookId);
+  console.log(book);
+  evt.target.reset();
+  refs.ulList.querySelector(`[data-id="${deleteBookId}"]`).remove();
+}
+async function onUpdateBookSubmit(evt) {
+  evt.preventDefault();
 
-    const formData = new FormData(evt.target);
-    const book = {}
-    
-    formData.forEach((value, key) => {
-        if (value) {
-            book[key] = value;
-        }
+  const formData = new FormData(evt.target);
+  const book = {};
 
-    })
+  formData.forEach((value, key) => {
+    if (value) {
+      book[key] = value;
+    }
+  });
 
-    const updateBookId = book.id; 
-    updateBook(updateBookId, book).then(book => {
-        const murkup = bookTamplate(book);
-     const updateBook = refs.ulList.querySelector(`[data-id="${updateBookId}"]`);
-        updateBook.insertAdjacentHTML('afterend', murkup);
-        updateBook.remove();
-    })
-    evt.target.reset();
-};
+  const updateBookId = book.id;
+  const book3 = await updateBook(updateBookId, book);
+  const murkup = bookTamplate(book3);
+  const updateBook = refs.ulList.querySelector(`[data-id="${updateBookId}"]`);
+  updateBook.insertAdjacentHTML('afterend', murkup);
+  updateBook.remove();
+  evt.target.reset();
+}
 
-function onResetBookSubmit(evt) {
-    evt.preventDefault();
-        const formData = new FormData(evt.target);
-    const book = {}
-    
-    formData.forEach((value, key) => {
-         
-            book[key] = value;
-        
-    })
-    resetBook(book.id, book).then(book => {
-        console.log(book);
-        const murkup = bookTamplate(book);
-        const bookReset = refs.ulList.querySelector(`[data-id="${book.id}"]`);
-        bookReset.insertAdjacentHTML('afterend', murkup);
-        bookReset.remove();
-    })
-    evt.target.reset();
+async function onResetBookSubmit(evt) {
+  evt.preventDefault();
+  const formData = new FormData(evt.target);
+  const book = {};
 
-};
+  formData.forEach((value, key) => {
+    book[key] = value;
+  });
+  const book2 = await resetBook(book.id, book);
+  console.log(book2);
+  const murkup = bookTamplate(book2);
+  const bookReset = refs.ulList.querySelector(`[data-id="${book2.id}"]`);
+  bookReset.insertAdjacentHTML('afterend', murkup);
+  bookReset.remove();
+
+  evt.target.reset();
+}
 
 getAllBooks().then(param => {
-    console.log(param);
-    renderAllBooks(param)
+  console.log(param);
+  renderAllBooks(param);
 });
 
-
-
 function renderAllBooks(books) {
-    const murkup = books.map(bookTamplate).join('');
-    refs.ulList.innerHTML = murkup;
+  const murkup = books.map(bookTamplate).join('');
+  refs.ulList.innerHTML = murkup;
 }
