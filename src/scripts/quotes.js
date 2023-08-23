0e69dca62b0838f974fe4e1fd871d275217ded9d
sstfnk3107 <s.stefanyk75@gmail.com>
2023-08-23 22:39:19 +0300

    123

--- src/scripts/quotes.js
+++ src/scripts/quotes.js
@@ -1,43 +1,36 @@
-import { getAllQuotes, getAuthorQuotes } from '../modules/quotes'
-
+import { getAllQuotes, getAuthorQuotes } from '../modules/quotes';
 
 const refs = {
-    ulList: document.querySelector('.js-quotes'),
-    form: document.querySelector('.js-form'),
+  ulList: document.querySelector('.js-quotes'),
+  form: document.querySelector('.js-form'),
 };
 
 refs.form.addEventListener('submit', onFormSubmit);
 
-function onFormSubmit(evt) {
-    evt.preventDefault();
-    const userInput = refs.form.elements.author.value;
-    if (userInput === '') {
-        getAllQuotes().then((quotes) => {
-    createMarkup(quotes)  
-});
-    } else {
-            getAuthorQuotes(userInput).then(quotes => {
-        createMarkup(quotes)
-        
-})
-    }
-    evt.target.reset();
-};
+async function onFormSubmit(evt) {
+  evt.preventDefault();
+  const userInput = refs.form.elements.author.value;
+  if (userInput === '') {
+    const quotes = await getAllQuotes();
+    createMarkup(quotes);
+  } else {
+    const quotes = await getAuthorQuotes(userInput);
+    createMarkup(quotes);
+  }
+  evt.target.reset();
+}
 
 function createMarkup(quotes) {
-    const markup = quotes.map(quotsTemplate).join('');
-    refs.ulList.innerHTML = markup;
-};
+  const markup = quotes.map(quotsTemplate).join('');
+  refs.ulList.innerHTML = markup;
+}
 
-getAllQuotes().then((quotes) => {
-    createMarkup(quotes)
-    
+getAllQuotes().then(quotes => {
+  createMarkup(quotes);
 });
 
-
-
-function quotsTemplate({id, author, body, tag, lang}) {
-    return `
+function quotsTemplate({ id, author, body, tag, lang }) {
+  return `
     <li data-id="${id}" class="book">
 <h3>${author}</h3>
 <p>${body}</p>
@@ -45,5 +38,5 @@ function quotsTemplate({id, author, body, tag, lang}) {
     <span>${tag}</span>
     <span>${lang}</span>
 </div>
-</li>`
-};
+</li>`;
+}
